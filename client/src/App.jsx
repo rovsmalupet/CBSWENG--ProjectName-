2254cceeaca43ba5537001a40db022e202281ee0
Matthew Fuentes <165547285+S180-TK@users.noreply.github.com>
2026-02-22 14:49:39 +0800

    Add Dashboard and Project Ledger
    New files in client/src/css:
    Dashboard.css
    Navbar.css
    ProjectLedger.css
    
    New files in client/src/pages:
    Dashboard.jsx
    ProjectLedger.jsx
    
    New file in client/src/components:
    Navbar.jsx
    
    Changes to:
    client/src/App.jsx
    client/package.json (something for react-dom?)
    client/package-lock.json (more react)
    
    Vite also copied client/public/js/script.js to client/dist/js/script.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
-import PostNewProject from './components/PostNewProject.jsx'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.jsx";
+import ProjectLedger from "./pages/ProjectLedger.jsx";
+import PostNewProject from "./components/PostNewProject.jsx";
 
 export default function App() {
   return (
-    <PostNewProject />
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/project-ledger" element={<ProjectLedger />} />
+        <Route path="/post-project" element={<PostNewProject />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
