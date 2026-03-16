acc1d48fb4753c731686d67aa4d304bf2afb9c7d
Rovick Dompor <rovick_dompor@dlsu.edu.ph>
2026-03-16 16:01:52 +0800

    fixed file name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ import EditProject from "./pages/EditProject.jsx"; // <-- added
 import AddContribution from "./pages/AddContribution.jsx"; // <-- added
 import AdminHomepage from "./pages/AdminHomepage.jsx";
 import ViewProjects from "./pages/viewProjects.jsx";
-import AdminProjectDetail from "./pages/AdminProjectDetail.jsx";
+import AdminProjectDetail from "./pages/Adminprojectdetail.jsx";
 import PendingAccounts from "./pages/PendingAccounts.jsx";
 
 export default function App() {
@@ -20,22 +20,19 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
-		
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/project-ledger" element={<ActiveProjects />} />
         <Route path="/unposted-projects" element={<UnapprovedProjects />} />
         <Route path="/post-project" element={<PostNewProject />} />
         <Route path="/edit-project/:id" element={<EditProject />} />{" "}
         <Route path="/add-contribution/:id" element={<AddContribution />} />{" "}
-		
         <Route path="/donor" element={<DonorHomepage />} />
         <Route path="/donor/bookmarks" element={<BookmarkedProjects />} />
         <Route path="/project/:id" element={<ProjectDetailPage />} />
-		
-		<Route path="/admin" element={<AdminHomepage />} />
-		<Route path="/viewProjects" element={<ViewProjects />} />
-		<Route path="/admin/project/:id" element={<AdminProjectDetail />} />
-		<Route path="/admin/pending-accounts" element={<PendingAccounts />} />
+        <Route path="/admin" element={<AdminHomepage />} />
+        <Route path="/viewProjects" element={<ViewProjects />} />
+        <Route path="/admin/project/:id" element={<AdminProjectDetail />} />
+        <Route path="/admin/pending-accounts" element={<PendingAccounts />} />
       </Routes>
     </BrowserRouter>
   );
